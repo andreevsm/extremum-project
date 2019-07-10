25699e4d5a8a3489420a38fd053f230ca266972b
aandreevsm <aandreev.sm@gmail.com>
2019-07-10 21:26:50 +0300

    Fixed bug

--- src/containers/layouts/App/index.js
+++ src/containers/layouts/App/index.js
@@ -5,7 +5,6 @@ import { ROUTES } from '../../../constants';
 import LoginPage from '../../pages/LoginPage';
 import SocialsPage from '../../pages/SocialsPage';
 import AppendBot from '../AppendBot';
-import LoginForm from '../../forms/LoginForm';
 import Game from '../../pages/Game';
 import './index.scss';
 
@@ -13,7 +12,6 @@ const App = () => (
   <div className="root">
     <BrowserRouter>
       <Route exact path={ROUTES.INITIAL} component={AppendBot} />
-      <Route path={ROUTES.LOGIN} component={LoginForm} />
       <Route path={ROUTES.GAME} component={Game} />
       <Route path={ROUTES.LOGIN} component={LoginPage} />
       <Route path={ROUTES.SOCIALS} component={SocialsPage} />
